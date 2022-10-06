eb0e5473ba8cc722bd25a15dfa55c766cd9d43e7
ryghi <ryghi5@gmail.com>
2022-10-06 12:34:18 -0600

    4th commit

--- app/src/main/java/com/example/android/weddingapp/createEvent/CreateEventFragment.kt
+++ app/src/main/java/com/example/android/weddingapp/createEvent/CreateEventFragment.kt
@@ -5,25 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.weddingapp.R
 import com.example.android.weddingapp.databinding.FragmentCreateEventBinding
+import com.example.android.weddingapp.eventList.EventDataItem
 import com.example.android.weddingapp.eventList.EventListViewModel
 
 
 class CreateEventFragment : Fragment() {
-    private lateinit var viewModel: EventListViewModel
 
     private lateinit var binding: FragmentCreateEventBinding
+
+    private lateinit var viewModel: CreateEventViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewModel = ViewModelProvider(this).get(CreateEventViewModel::class.java)
+
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_create_event, container, false)
+        val event = binding.event
+        binding.saveButton.setOnClickListener{
+
+            if (validate(event)){
+                viewModel.saveEvent(event)
+                Toast.makeText(activity, "Event saved!", Toast.LENGTH_LONG).show()
+            }
+        }
 
         return binding.root
+    }
+
+    private fun validate(event: EventDataItem?): Boolean {
+
+        if (event == null || event.description.isNullOrEmpty() || event.time.isNullOrEmpty() ||
+            event.title.isNullOrEmpty() || event.location.isNullOrEmpty()) {
+            Toast.makeText(activity, "Fill out all fields to create an event", Toast.LENGTH_LONG).show()
+            return false
+        }
+        return true
     }
 
 }
--- app/src/main/java/com/example/android/weddingapp/createEvent/CreateEventViewModel.kt
+++ app/src/main/java/com/example/android/weddingapp/createEvent/CreateEventViewModel.kt
@@ -1,21 +1,26 @@
 package com.example.android.weddingapp.createEvent
 
 import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.weddingapp.data.EventDataBase
 import com.example.android.weddingapp.data.EventRepository
 import com.example.android.weddingapp.eventList.EventDataItem
+import kotlinx.coroutines.launch
 
-class CreateEventViewModel(
-    application: Application,
-    val title: String,
-    val description: String,
-    val time: String,
-    val location: String
-) {
+class CreateEventViewModel(application: Application) : AndroidViewModel(application) {
+
     private val repo = EventRepository(EventDataBase.getInstance(application).dao)
-
-    suspend fun saveEvent(event: EventDataItem) {
-        repo.saveEvent(event)
+    val TAG = this.javaClass.simpleName
+    fun saveEvent(event: EventDataItem?) {
+        if (event != null) {
+            viewModelScope.launch { repo.saveEvent(event) }
+        }
+        else {
+            Log.e(TAG, "saveEvent: event to be saved was null")
+        }
     }
 
 }
